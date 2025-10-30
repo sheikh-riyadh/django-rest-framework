3cb8d22eb2b920ac13bdf6cf478cefa2ef529d9d
sheikh-riyadh <sheikhriyad350883@gmail.com>
2025-10-30 07:57:17 +0600

    working on django-rest-framework

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from product.views import ProductViewSet, ReviewViewSet
+from rest_framework_nested import routers
+
+router = routers.DefaultRouter()
+router.register('products', ProductViewSet)
+router.register('review', ReviewViewSet)
+
+product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+product_router.register('review', ReviewViewSet, basename='product-review')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(product_router.urls))
+]
---
+++ product/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='product/', default='product/default-image.png')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
